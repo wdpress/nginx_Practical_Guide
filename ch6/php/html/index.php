20aebe86887f0e5c69e2ace0e36ac64040f99b12
Shunsuke MICHII <harukasan@me.com>
2015-12-15 00:39:13 +0900

    Chapter 6: Impl PHP samples.

---
+++ ch6/php/html/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="/application.css" />
+  </head>
+  <body>
+    <?php echo "Hello, world!"; ?>
+    <ul>
+      <li><a href="phpinfo.php">phpinfo.php</a>
+    </ul>
+  </body>
+</html>
