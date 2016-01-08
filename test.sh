9bf35c110d04136a75a363717d933d82bdb8f196
Shunsuke MICHII <harukasan@me.com>
2016-01-08 14:41:33 +0900

    Chapter 7: Refine tests.

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 nginx=/usr/local/nginx/sbin/nginx
+resty=
 
 # Chapter 3
 cd ch3
@@ -17,21 +18,41 @@ sudo $nginx -t -p $(pwd) -c nginx/3.3-list3.7-nginx.conf
 sudo $nginx -t -p $(pwd) -c nginx/3.4-performance_tuning.conf
 
 # Chapter 4
-
+cd ../ch4
+sudo $nginx -t -p $(pwd) -c nginx/4.1-index_and_error_page.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.1-list4.1-static_server.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.1-list4.2-location_directive.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.1-list4.3-nested_location.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.2-basic_authentication.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.2-list4.4-black_list.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.2-list4.5-white_list.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.2-list4.6-restrictions.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.2-list4.8-limit_by_remote_addr.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.2-list4.9-request_limit.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.3-list4.10-rewrite.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.3-return.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.3-valid_referers.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.4-gzip_static.conf
+sudo $nginx -t -p $(pwd) -c nginx/4.4-list4.11-gzip.conf
 
 # Chapter 5
-
-
-# Chapter 6
-
+cd ../ch5
+sudo $nginx -t -p $(pwd) -c nginx/nginx.conf
 
 # Chapter 7
-
+cd ../ch7
 
 # Chapter 8
-
+cd ../ch8
+sudo $nginx -t -p $(pwd) -c nginx/combined-log-nginx.conf
+sudo $nginx -t -p $(pwd) -c nginx/ltsv-log-nginx.conf
+sudo $nginx -t -p $(pwd) -c nginx/stub_status_setting-nginx.conf
+sudo $nginx -t -p $(pwd) -c nginx/tsv-log-nginx.conf
 
 # Chapter 9
-
+cd ../ch9
+# sudo $resty -t -p $(pwd) -c nginx/...
 
 # Chapter 10
+cd ../ch10
+# sudo $resty -t -p $(pwd) -c nginx/...
